5dc7abe85b992785ae25e924d71bdff3591d1b1a
nikoparas1 <nikoparas1@gmail.com>
2025-02-13 20:59:19 -0500

    fixed issue with tls certificate

--- api/db.py
+++ api/db.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
+import certifi
 
 load_dotenv(dotenv_path=".env.local")
 
@@ -11,7 +12,9 @@ MONGODB_USER = os.getenv("MONGODB_USER")
 MONGODB_PASSWORD = os.getenv("MONGODB_PASSWORD")
 MONGODB_CLUSTER = os.getenv("MONGODB_CLUSTER")
 uri = f"mongodb+srv://{MONGODB_USER}:{MONGODB_PASSWORD}@{MONGODB_CLUSTER}/?retryWrites=true&w=majority&appName=Cluster0"
-client = MongoClient(uri, tls=True, server_api=ServerApi("1"))
+client = MongoClient(
+    uri, tls=True, tlsCAFile=certifi.where(), server_api=ServerApi("1")
+)
 db = client["math_templates_db"]
 templates_collection = db["templates"]
 
