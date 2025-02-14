026130f8445ce81d6de4b6e1d0e92a44fde2fe94
nikoparas1 <nikoparas1@gmail.com>
2025-02-13 20:35:16 -0500

    fix mongo uri formatting

--- api/db.py
+++ api/db.py
@@ -10,7 +10,7 @@ load_dotenv(dotenv_path=".env.local")
 MONGODB_USER = os.getenv("MONGODB_USER")
 MONGODB_PASSWORD = os.getenv("MONGODB_PASSWORD")
 MONGODB_CLUSTER = os.getenv("MONGODB_CLUSTER")
-uri = f"mongodb+srv://{MONGODB_USER}:{MONGODB_PASSWORD}@{MONGODB_CLUSTER}?retryWrites=true&w=majority&appName=Cluster0"
+uri = f"mongodb+srv://{MONGODB_USER}:{MONGODB_PASSWORD}@{MONGODB_CLUSTER}/?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(uri, server_api=ServerApi("1"))
 db = client["math_templates_db"]
 templates_collection = db["templates"]
